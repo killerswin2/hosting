1c1267ae94c521d38c6da496e7b612dbf67177ac
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-06-28 14:50:05 -0500

    first release

--- src/binding/binding.hpp
+++ src/binding/binding.hpp
@@ -1,17 +1,30 @@
-#pragma once
+#ifndef _BINDING_H
+#define _BINDING_H
 
 #include <iostream>
 #include <cstdlib>
 #include <filesystem>
+#include <cassert>
 
 #include <nethost.h>
 #include <coreclr_delegates.h>
 #include <hostfxr.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#define STR(s) L ## s
+#define CH(c) L ## c
+#define DIR_SEPARATOR L'\\'
+
+#else
 #include <dlfcn.h>
 #include <climits>
-#include <cassert>
+#define STR(s) s
+#define CH(c) c
+#define DIR_SEPARATOR '/'
+#define MAX_PATH PATH_MAX
 
+#endif
 
 #include <string>
 #include <filesystem>
@@ -27,51 +40,31 @@ private:
 
     // used for custom delegate types
     string_t _assemblyStaticClassName;
-    
+
     std::filesystem::path _assemblyPath;
     std::function<int(
-        const char_t *assembly_path,    
-        const char_t *type_name,   
-        const char_t *method_name,
-        const char_t *delegate_type_name,
-        void *reserved,           
-        void **delegate
-    )> _load_assembly_and_get_function_pointer;
+        const char_t* assembly_path,
+        const char_t* type_name,
+        const char_t* method_name,
+        const char_t* delegate_type_name,
+        void* reserved,
+        void** delegate
+        )> _load_assembly_and_get_function_pointer;
 
 public:
     Bindings() {}
     Bindings(string_t& assemblyName, string_t assemblyStaticClassName, std::filesystem::path& assemblyPath, load_assembly_and_get_function_pointer_fn functionPointer);
     template<typename t1>
-    t1 get_function_pointer_from_assembly(const string_t & methodName);
+    t1 get_function_pointer_from_assembly(const string_t& methodName, const string_t& customDelegate);
 };
-
-Bindings::Bindings(string_t& assemblyName, string_t assemblyStaticClassName, std::filesystem::path& assemblyPath, load_assembly_and_get_function_pointer_fn functionPointer) : _assemblyName{std::move(assemblyName)}, _assemblyStaticClassName{std::move(assemblyStaticClassName)}
-{
-    _assemblyPath.clear();
-    // check that the path exists
-    assemblyPath.append(_assemblyName+".dll");
-    if(std::filesystem::exists(assemblyPath))
-    {
-        _assemblyPath = std::filesystem::canonical(assemblyPath);
-        std::cout << "Path: " << _assemblyPath << "\n";
-    }
-
-
-    if(_assemblyPath.empty())
-    {
-        std::cout << "Path is empty\n";
-    }
-
-    _load_assembly_and_get_function_pointer = std::move(functionPointer);
-}
 
 //@TODO: make this cached.
 template<typename t1>
-t1 Bindings::get_function_pointer_from_assembly(const string_t & methodName)
+t1 Bindings::get_function_pointer_from_assembly(const string_t& methodName, const string_t& customDelegate)
 {
 
-    const string_t dotnetType = _assemblyName + "." + _assemblyStaticClassName + ", " + _assemblyName;
-    const string_t delegateTypeName = _assemblyName + "." + _assemblyStaticClassName + "+" + "doMathDelegate" +", " + _assemblyName;
+    const string_t dotnetType = _assemblyName + STR(".") + _assemblyStaticClassName + STR(", ") + _assemblyName;
+    const string_t delegateTypeName = _assemblyName + STR(".") + _assemblyStaticClassName + STR("+") + customDelegate + STR(", ") + _assemblyName;
 
     t1 custom = nullptr;
     int rc = _load_assembly_and_get_function_pointer(
@@ -82,11 +75,12 @@ t1 Bindings::get_function_pointer_from_assembly(const string_t & methodName)
         nullptr,
         (void**)&custom
     );
-    
-    if(rc != 0)
+
+    if (rc != 0)
     {
         std::cout << "Error code: " << rc << "\n";
     }
     assert(rc == 0 && custom != nullptr && "Failure: load_assembly_and_get_function_pointer()");
     return custom;
 }
+#endif
--- src/library/library.cpp
+++ src/library/library.cpp
@@ -1,12 +1,24 @@
 #include "library.hpp"
 
 
+#ifdef _WIN32
 void Library::load_library()
 {
-    if(std::filesystem::exists(_path))
+    if (std::filesystem::exists(_path))
     {
-        void *h = dlopen(_path.c_str(), RTLD_LAZY | RTLD_LOCAL);
+        HMODULE h = ::LoadLibraryW(_path.c_str());
         assert(h != nullptr);
         _library = h;
-    } 
+    }
 }
+#else
+void Library::load_library()
+{
+    if (std::filesystem::exists(_path))
+    {
+        void* h = dlopen(_path.c_str(), RTLD_LAZY | RTLD_LOCAL);
+        assert(h != nullptr);
+        _library = h;
+    }
+}
+#endif
